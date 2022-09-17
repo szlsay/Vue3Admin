67df7cac17565329a52c41874a21b813d009f132
小沈曰 <409178030@qq.com>
2022-09-17 18:15:56 +0800

    api

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,18 @@ function resolve(dir) {
 }
 module.exports = defineConfig({
   transpileDependencies: true,
+  devServer: {
+    // 配置反向代理
+    proxy: {
+      // 当地址中有/api的时候会触发代理机制
+      '/api': {
+        // 要代理的服务器地址  这里不用写 api
+        target: 'https://api.imooc-admin.lgdsunday.club/',
+        // target: 'http://127.0.0.1:3004/',
+        changeOrigin: true // 是否跨域
+      }
+    }
+  },
   chainWebpack(config) {
     // 设置 svg-sprite-loader
     // config 为 webpack 配置对象
